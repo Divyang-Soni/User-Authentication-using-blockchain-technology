69ba3c52b05f439cdcf7fde9657c0c101c51e381
Raj Baraiya <rajmansukhbhai.baraiya@sjsu.edu>
2018-11-02 23:12:31 -0700

    typo

--- dbscript.sql
+++ dbscript.sql
@@ -9,4 +9,4 @@ sudo -u raj psql //connects postgres with this user
 
 CREATE TABLE company_details(cid serial PRIMARY KEY, name varchar(100), address varchar(200), city varchar(50), state varchar(50), country varchar(50), zip int, phone varchar(50), headquarter varchar(50), founded_date date, organization_type varchar(50), size int, created_date date, created_by int, modified_date date, modified_by int);
 
-CREATE TABLE company_branch(bid serial PRIMARY KEY, pid int NOT NULL, address varchar(200), city varchar(50), state varchar(50), country varchar(50), zip int, phone varchar(50), created_date date, created_by int, modified_date date, modified_by int);
+CREATE TABLE company_branch(bid serial PRIMARY KEY, cid int NOT NULL, address varchar(200), city varchar(50), state varchar(50), country varchar(50), zip int, phone varchar(50), created_date date, created_by int, modified_date date, modified_by int);
