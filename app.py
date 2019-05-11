13a46db252eb801c5edd094f49d6bac1eede83a3
Divyang Soni <divyang8842@gmail.com>
2019-05-10 19:55:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, Session
 from util.util import parse_config
 import os
 from flask_cors import CORS
+from datetime import timedelta
 
 app = Flask(__name__)
 config = parse_config()
@@ -10,8 +11,8 @@ sess = Session()
 
 if __name__ == '__main__' or __name__ == 'app':
     app.secret_key = 'cmpe295'
-    app.config['SESSION_TYPE'] = 'filesystem'
-
+    #app.config['SESSION_TYPE'] = 'filesystem'
+    app.permanent_session_lifetime = timedelta(minutes=5)
     app.register_blueprint(user)
     port = int(os.environ.get('PORT', config['server']['port']))
     CORS(app, support_credentials=True)
